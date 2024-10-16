399f106d3e49321504399f7ae4051dda5d75227d
humayra-neela <neela1578@gmail.com>
2024-10-16 14:55:38 +0400

    Create Coding is cool.py
    1st work

---
+++ Exercise01/Coding is cool.py
@@ -0,0 +1,12 @@
+# Our first word in Python learning word01
+word01 = "Coding"
+
+# Using the first word to the vaiable word02
+word02 = "is"
+
+# Using the first word to variable word03
+word03 = "cool"
+
+#learning to get result
+sentence = word01 + " " + word02 + " " + word03
+print(sentence)
