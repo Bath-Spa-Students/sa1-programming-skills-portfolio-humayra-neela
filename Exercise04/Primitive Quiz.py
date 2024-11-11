8e687d3a5b21a012667e73fdeb8cffd209fd8300
humayra-neela <neela1578@gmail.com>
2024-11-11 10:54:01 +0400

    Primitive_quiz.py

---
+++ Exercise04/Primitive Quiz.py
@@ -0,0 +1,10 @@
+#Creating a Program asking Questions.
+
+#Assigning The Question
+Answer = input("What is the capital of France? :")
+
+#Giving Conditions for the Answer
+if Answer == "Paris": 
+    print ("The answer is correct.")
+else:
+    print ("The answer is wrong")
