7608d77d9f0f280f704ec17dad1e50a0dac63d07
humayra-neela <neela1578@gmail.com>
2024-11-11 10:09:35 +0400

    Create Simple Sums.py

---
+++ Exercise02/Simple Sums.py
@@ -0,0 +1,13 @@
+#Creating some integer variables to perform arithmetic operations
+
+#Adding the first variable
+Num01 = (int(8))
+
+#Adding the second variable
+Num02 = (int(10))
+
+#Adding a third variable which is the sum of the 1st two variables
+Total_Num = (Num01 + Num02)
+
+#printing the final result
+print(Total_Num)
