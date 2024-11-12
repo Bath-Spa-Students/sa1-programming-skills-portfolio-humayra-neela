20325db284a11d20cfe9ae8e52a4b446e35fcf66
humayra-neela <neela1578@gmail.com>
2024-11-12 18:57:17 +0400

    Create Biography.py

---
+++ Exercise03/Biography.py
@@ -0,0 +1,7 @@
+#Creating a Programme using Python Dictionary
+
+#Putting Information in the Dictionary Statement.
+Information = { "Name": "Humayra Afroj Neela", "Hometown": "Bangladesh", "Age": 20}
+
+# Printing the information using single Print() Statement.
+print(f"Name: {Information['Name']}\nHometown: {Information['Hometown']}\nAge: {Information['Age']}")
