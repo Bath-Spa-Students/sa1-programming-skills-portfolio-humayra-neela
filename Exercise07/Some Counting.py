858f8a102f5507dd976f940e04a2125231b070b1
humayra-neela <neela1578@gmail.com>
2024-11-14 11:10:34 +0400

    Create Some Counting.py

---
+++ Exercise07/Some Counting.py
@@ -0,0 +1,26 @@
+# Using newly acquired knowledge in 'for loop' to do some Countings.
+
+# 1. used for loop to count up from 0 to 50 in increments of 1.
+print("Counting up from 0 to 50 **in increments of 1**:")
+for Nobita in range(0, 51, 1):
+    print(Nobita)
+
+# 2. used for loop to count down from 50 to 0 in decrements of 1
+print("\nCounting down from 50 to 0 **in decrements of 1**:")
+for Gian in range(50, -1, -1):
+    print(Gian)
+
+# 3. used for loop to count up from 30 to 50 in increments of 1
+print("\nCounting up from 30 to 50 **in increments of 1**:")
+for Sunio in range(30, 51, 1):
+    print(Sunio)
+
+# 4. used for loop to count down from 50 to 10 in decrements of 2
+print("\nCounting down from 50 to 10 **in decrements of 2**:")
+for Shizuka in range(50, 9, -2):
+    print(Shizuka)
+
+# 5. used for loop to count up from 100 to 200 in increments of 5
+print("\nCounting up from 100 to 200 **in increments of 5**:")
+for Doraemon in range(100, 201, 5):
+    print(Doraemon)
