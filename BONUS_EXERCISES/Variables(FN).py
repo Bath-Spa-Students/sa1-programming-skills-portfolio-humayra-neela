3ac819911db77ea0755f396811fddcd54717c196
humayra-neela <neela1578@gmail.com>
2024-11-20 15:44:40 +0400

    Create Variables(FN).py

---
+++ BONUS_EXERCISES/Variables(FN).py
@@ -0,0 +1,8 @@
+#Crating a Code that prints my Favourite Number
+
+# Inputing my favourite Number
+Number = 1578
+
+# Using that Variable to reveal my favourite number
+Number = f"My Favourite Number is {Number}."
+print(Number)
