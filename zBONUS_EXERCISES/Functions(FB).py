457d605b4a641511255434d4fcc2c8e82767716f
humayra-neela <neela1578@gmail.com>
2024-11-22 03:22:06 +0400

    Create Functions(FB).py

---
+++ zBONUS_EXERCISES/Functions(FB).py
@@ -0,0 +1,5 @@
+# Creating a function which accepts one parameter
+def favourite_book(title):
+    print(f"One of my favourite books is {title}")
+#including the book title as an argument
+favourite_book("Aporajita by Rabindranath tagor")
