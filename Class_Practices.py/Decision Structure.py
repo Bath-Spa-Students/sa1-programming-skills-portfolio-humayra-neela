5d10f04f2722659d4520c778c165c8d18a86fb54
humayra-neela <neela1578@gmail.com>
2024-11-22 03:12:57 +0400

    Create Decision Structure.py

---
+++ Class_Practices.py/Decision Structure.py
@@ -0,0 +1,11 @@
+# Ask the user for input
+Salary = int (input ("Please input your salary: "))
+# profit variable is set to 0
+profit = 0
+#using if-else statemnet for dicision making
+if Salary > 999:
+    profit = 444 
+else : 
+    profit = 0
+
+print("This month your profit is "+str(profit))
