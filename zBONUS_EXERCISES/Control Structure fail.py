01a1a3cfb72a32164756ab6ea06788f8f0e3d69f
humayra-neela <neela1578@gmail.com>
2024-11-22 03:22:46 +0400

    Create Control Structure fail.py

---
+++ zBONUS_EXERCISES/Control Structure fail.py
@@ -0,0 +1,8 @@
+#Creating a programme which fails the if test
+
+#Inputing a different value in the variable
+alien_color = "yellow"
+
+if alien_color == 'green': #Using if structure for the demand
+    print("Congratulations!You have earned 5 points")
+# Not using any eles function so if the value doesn't matches with the demand there will not be any output
