9e21a4aab3d02ebdc4cbff7f495bc259cc54187e
humayra-neela <neela1578@gmail.com>
2024-11-22 03:22:20 +0400

    Create Dictionaries.py

---
+++ zBONUS_EXERCISES/Dictionaries.py
@@ -0,0 +1,13 @@
+# Creating a python dictionary with the information of a friend
+
+#inputing my friends information in a dictionary
+Friend = {"first_name" : "Suha",
+          "last_name" : "Fatima",
+          "Age" : 18,
+          "city" : "Dubai"}
+
+#printing each information one by one
+print(f"My friends first name is, {Friend['first_name']}")
+print(f"My friends last name is, {Friend['last_name']}")
+print(f"My friend is {Friend['Age']} years old")
+print(f"My Friend lives in {Friend['city']}")
