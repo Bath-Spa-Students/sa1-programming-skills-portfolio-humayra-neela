2a0791fa0af585cc3a889c4afb18d2761ef76529
humayra-neela <neela1578@gmail.com>
2024-11-22 03:31:45 +0400

    Delete Control Structure pass.py

--- BONUS_EXERCISES/Control Structure pass.py
+++
@@ -1,6 +0,0 @@
-# Creating a programme which passes the if test
-
-alien_color = "green" #Giving the color of that imaginery alien
-
-if alien_color == 'green':  #Using if structure
-    print("Congratulations!You have earned 5 points")
