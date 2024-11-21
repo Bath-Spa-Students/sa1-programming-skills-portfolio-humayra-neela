399c88a06dcda3b66eace2f1d9e99a6a0d9a1615
humayra-neela <neela1578@gmail.com>
2024-11-22 03:32:59 +0400

    Delete Variables(FN).py

--- BONUS_EXERCISES/Variables(FN).py
+++
@@ -1,8 +0,0 @@
-#Crating a Code that prints my Favourite Number
-
-# Inputing my favourite Number
-Number = 1578
-
-# Using that Variable to reveal my favourite number
-Number = f"My Favourite Number is {Number}."
-print(Number)
