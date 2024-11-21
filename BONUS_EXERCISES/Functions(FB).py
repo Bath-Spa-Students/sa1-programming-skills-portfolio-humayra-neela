6bbd2a8fc350dd008c4650d281ce2931de73af4d
humayra-neela <neela1578@gmail.com>
2024-11-22 03:32:09 +0400

    Delete Functions(FB).py

--- BONUS_EXERCISES/Functions(FB).py
+++
@@ -1,5 +0,0 @@
-# Creating a function which accepts one parameter
-def favourite_book(title):
-    print(f"One of my favourite books is {title}")
-#including the book title as an argument
-favourite_book("Aporajita by Rabindranath tagor")
