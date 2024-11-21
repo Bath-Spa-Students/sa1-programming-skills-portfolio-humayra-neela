f502bea6346c86546cfc3deac7580d07c03b93b2
humayra-neela <neela1578@gmail.com>
2024-11-22 03:22:56 +0400

    Create Control Structure pass.py

---
+++ zBONUS_EXERCISES/Control Structure pass.py
@@ -0,0 +1,6 @@
+# Creating a programme which passes the if test
+
+alien_color = "green" #Giving the color of that imaginery alien
+
+if alien_color == 'green':  #Using if structure
+    print("Congratulations!You have earned 5 points")
