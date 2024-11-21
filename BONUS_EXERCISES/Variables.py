c61ab8d404a55a63e5b69ba01b2a6c987ea09957
humayra-neela <neela1578@gmail.com>
2024-11-22 03:33:15 +0400

    Delete Variables.py

--- BONUS_EXERCISES/Variables.py
+++
@@ -1,9 +0,0 @@
-# Assigning and changing Variable values
-
-# first Assigning message in a variable
-Humayra = "This user is a Student of BathSpa University." 
-print(Humayra)
-
-# secondly changing the message in that variable
-Humayra = "She is studying Creative Computing."
-print(Humayra)
